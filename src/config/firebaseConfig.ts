dde271a8d3f8b6e148edd50b59ba9502be1bcfe1
suhail <demonbruster@gmail.com>
2024-03-05 10:54:06 +0530

    feat: firebase config to .env

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import { NEXT_PUBLIC_FIREBASE_API_KEY, NEXT_PUBLIC_FIREBASE_APP_ID, NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID, NEXT_PUBLIC_FIREBASE_PROJECT_ID, NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET } from "@/constants";
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
@@ -9,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBOGSResIZwIALSJvWWk0FBRsqkZ7f4q74",
-  authDomain: "open-retro-7e232.firebaseapp.com",
-  projectId: "open-retro-7e232",
-  storageBucket: "open-retro-7e232.appspot.com",
-  messagingSenderId: "933621571253",
-  appId: "1:933621571253:web:ad99c8f4de08bf572228fc",
-  measurementId: "G-HW09TQ2D4C"
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
